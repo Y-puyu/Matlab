ae31332a7413c92304417efa8bd9b6d7a98d38ad
YangLi <345462072@qq.com>
2019-04-08 21:49:27 +0800

    LP

--- LinearProgramming.m
+++ LinearProgramming.m
@@ -5,3 +5,12 @@ aeq = [1,1,1];
 beq = 7;
 [x, y] = linprog(f, a, b, aeq, beq, zeros(3,1));
 a
+
+
+f = [3; -4; 2; -5];
+a = [1, 1, 3, -1; 2, -3, 1, -2];
+b= [14; -2];
+aeq = [4, -1, 2, -1];
+beq = -2;
+[x, y] = linprog(f, a, b, aeq, beq, zeros(3,1));
+x, y = -y
